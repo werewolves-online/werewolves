21f7a5061e373deab46c4364d9edcef7027cc1f3
Ammar Hussein <ammar.atef45@gmail.com>
2022-04-23 19:40:04 -0700

    create and deploy template app (#1)

---
+++ werewolves/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:werewolves/ui/about_us.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Werewolves App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AboutUsPage(),
+    );
+  }
+}
