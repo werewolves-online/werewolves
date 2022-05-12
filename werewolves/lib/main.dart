12dadee25d887c233088458f087432cc232c8dc9
david-barrette <davidbar968@gmail.com>
2022-05-11 22:33:14 -0700

    Added an AppBar and updated tests

--- werewolves/lib/main.dart
+++ werewolves/lib/main.dart
@@ -11,11 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Werewolves App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: AboutUsPage(),
-    );
+        key: const Key("main_MaterialApp"),
+        title: 'Werewolves App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          key: const Key("main_Scaffold"),
+          appBar: AppBar(
+              key: const Key("main_AppBar"),
+              title: const Center(child: Text('Werewolves App'))),
+          body: const AboutUsPage(),
+        ));
   }
 }
