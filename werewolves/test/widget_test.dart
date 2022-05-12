e14cd55c88f712f7557175974879786141dd6b85
david-barrette <75652896+david-barrette@users.noreply.github.com>
2022-05-12 13:13:46 -0700

    Added an AppBar and updated tests (#7)

--- werewolves/test/widget_test.dart
+++ werewolves/test/widget_test.dart
@@ -11,20 +11,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:werewolves/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Werewolf App smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    // Expected Text
+    expect(find.text('Werewolves App'), findsOneWidget);
+
+    //Expected Widgets
+    expect(find.byKey(const Key("main_MaterialApp")), findsOneWidget);
+    expect(find.byKey(const Key("main_Scaffold")), findsOneWidget);
+    expect(find.byKey(const Key("main_AppBar")), findsOneWidget);
+
+    //Things that should not be here
     expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
