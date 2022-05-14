a4b301f63881d65f23c45712dd7127cd1d64ca6d
Ammar Hussein <ammar.atef45@gmail.com>
2022-05-14 13:37:09 -0700

    added google signin

--- werewolves/lib/main.dart
+++ werewolves/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:werewolves/ui/about_us.dart';
+import 'package:werewolves/ui/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AboutUsPage(),
+      home: const LoginPage(),
     );
   }
 }
---
+++ werewolves/lib/ui/login.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:werewolves/ui/widgets/round_button.dart';
+import 'package:werewolves/utils/authentication.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&w=1000&q=80'),
+                  fit: BoxFit.fill),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 6,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 60, left: 25),
+                  child: Column(
+                    children: const [
+                      Text(
+                        'Hello',
+                        style: TextStyle(
+                            fontSize: 55,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      Text(
+                        'Welcome to Werewolves APP, let\'s get you started',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: double.infinity,
+                      padding:
+                          const EdgeInsets.only(top: 25, left: 24, right: 24),
+                      child: RoundButtonWithLogo(
+                        imageProvider:
+                            const AssetImage("assets/google_logo.png"),
+                        text: 'Sign in with Google',
+                        onPressed: () async {
+                          final user = await Authentication.signInWithGoogle(
+                            context: context,
+                          );
+                          print(user);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
