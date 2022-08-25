9ac42fa14a69b2d097b42e1258cabd485f552d87
NiharS <niharrsheth@gmail.com>
2022-08-24 21:12:24 -0700

    update firebase and dart sdk (#19)
    * update firebase and dart sdk
    
    * removed unused import
    
    Co-authored-by: Ammar Hussein <ammar.atef45@gmail.com>

--- werewolves/lib/utils/authentication.dart
+++ werewolves/lib/utils/authentication.dart
@@ -1,18 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class Authentication {
-  static Future<FirebaseApp> initializeFirebase() async {
-    FirebaseApp firebaseApp = await Firebase.initializeApp();
-
-    // TODO: Add auto login logic
-
-    return firebaseApp;
-  }
-
   static Future<User?> signInWithGoogle({required BuildContext context}) async {
     FirebaseAuth auth = FirebaseAuth.instance;
     User? user;
